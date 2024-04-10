21184e9837b0c7f792207aaef3fa23ed943991b4
HSouch <harrison.souchereau@yale.edu>
2024-04-09 21:07:25 -0400

    Changes to config file format

--- galprime/config.py
+++ galprime/config.py
@@ -2,30 +2,42 @@ from configobj import ConfigObj
 
 
 
-def dump_default_config_file():
+def dump_default_config_file(outname="default.gprime"):
 
 
     config = ConfigObj()
-    config.filename = "default.gprime"
+    config.filename = outname
+    config["FILE_DIR"] = ""
 
     config["FILES"] = {}
     config["FILES"]["CATALOGUE"] = "cat.fits"
     config["FILES"]["PSFS"] = "psfs.fits"
     config["FILES"]["BACKGROUNDS"] = "backgrounds.fits"
+    config["FILES"]["MAG_CATALOGUE"] = None
 
     config["KEYS"] = {}
+    config["KEYS"]["RA"] = "RA_1"
+    config["KEYS"]["DEC"] = "DEC_1"
     config["KEYS"]["MAG"] = "i"
     config["KEYS"]["R50"] = "R_GIM2D"
     config["KEYS"]["N"] = "SERSIC_N_GIM2D"
     config["KEYS"]["ELLIP"] = "ELL_GIM2D"
 
+    config["PSF_INFO"] = {}
+    config["PSF_INFO"]["PSF_RA"] = "RA"
+    config["PSF_INFO"]["PSF_DEC"] = "DEC"
+
     config["BINS"] = {}
-    config["Z_BEST"] = [0.1, 0.3, 0.5, 0.7, 0.9]
-    config["MASS_MED"] = [10, 10.5, 11, 11.5]
-    config["sfProb"] = [0, 0.5, 1.]
+    config["BINS"]["Z_BEST"] = [0.1, 0.3, 0.5, 0.7, 0.9]
+    config["BINS"]["MASS_MED"] = [10, 10.5, 11, 11.5]
+    config["BINS"]["sfProb"] = [0, 0.5, 1.]
 
     config["DIRS"] = {}
     config["DIRS"]["OUTDIR"] = "gprime_out/"
 
     config.write()
 
+
+def read_config_file(filename):
+    config = ConfigObj(filename)
+    return config
