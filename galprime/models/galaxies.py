4473e838f58b7a977fba1fb0f223fe7e111cbf15
HSouch <harrison.souchereau@yale.edu>
2024-08-12 14:26:42 -0300

    Added model gen methods

--- galprime/models/galaxies.py
+++ galprime/models/galaxies.py
@@ -4,6 +4,7 @@ from astropy.modeling.models import Sersic2D
 import numpy as np
 
 from .. import utils
+from .. import cutouts
 
 
 class GalaxyModel:
@@ -17,6 +18,9 @@ class GalaxyModel:
         raise NotImplementedError("Abstract class")
 
     def generate(self):
+        raise NotImplementedError("Abstract class")
+    
+    def gen_multiple(self, config, kde, n_models=100, **kwargs):
         raise NotImplementedError("Abstract class")
 
 
@@ -36,6 +40,54 @@ class SingleSersicModel(GalaxyModel):
         z *= ltot / np.nansum(z)
 
         return z
+    
+    def verify_params(self, mag, r_eff, n, ellip):
+        good = (0.1 < n < 6)
+        good = good and (0.1 < ellip < 0.9)
+        return good
+    
+    
+    def gen_multiple(self, config, kde, mag_kde=None, n_models=100, **kwargs):
+        max_tries = kwargs.get("ntries", 100)
+        arcconv = float(config["MODEL"]["ARCCONV"])
+        size = float(config["MODEL"]["SIZE"])
+        
+        zpm = float(config["MODEL"]["ZPM"])
+
+        xs, ys = np.mgrid[:size, :size]
+
+        models, models_info = [], []
+        for _ in range(n_models):
+            try_index = 0
+
+            while try_index < max_tries:
+                mag, r_eff, n, ellip = kde.resample(size=1)
+                mag, r_eff, n, ellip = mag[0], r_eff[0], n[0], ellip[0]
+
+                if self.verify_params(mag, r_eff, n, ellip):
+                    break
+                else:
+                    try_index += 1
+
+            if mag_kde is not None:
+                mag = mag_kde.resample(size=1)[0]
+            
+            r_eff_pix = r_eff / arcconv
+            Ltot = utils.Ltot(mag, zpm)
+
+            theta = np.random.uniform(0, 2 * np.pi)
+
+            model = Sersic2D(amplitude=1, r_eff=r_eff_pix, n=n, x_0=size/2, y_0=size/2, ellip=ellip, theta=theta)
+            z = model(xs, ys)
+
+            z *= Ltot / np.nansum(z)
+
+            model_info = {"MAG": mag, "R50": r_eff, "N": n, "ELLIP": ellip, "R50_PIX": r_eff_pix}
+
+            models.append(z)
+            models_info.append(model_info)
+    
+        return cutouts.Cutouts(cutouts=models, cutout_data=models_info)
     
 
 class BDSersicModel(GalaxyModel):
