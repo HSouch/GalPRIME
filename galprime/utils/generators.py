e507bce1f06b669de4a7b1fd4f5417c8d2d3e58e
HSouch <harrison.souchereau@yale.edu>
2024-08-12 12:55:04 -0300

    Changed normalization to sersic model generation

--- galprime/utils/generators.py
+++ galprime/utils/generators.py
@@ -5,6 +5,7 @@ from astropy.modeling.models import Sersic2D
 from ..cutouts import Cutouts
 from .fluxes import I_e
 
+from . import fluxes
 
 
 def gen_sersic_models(n_models = 50, width=(81, 81),
@@ -24,10 +25,14 @@ def gen_sersic_models(n_models = 50, width=(81, 81),
         re = np.random.uniform(*re_range)
         ellip = np.random.uniform(0.2, 0.8)
         pa = np.random.uniform(0, 2 * np.pi)
-        
-        sersic = Sersic2D(amplitude=I_e(mag, re, n), r_eff=re, n=n, x_0=width[1]/2, y_0=width[0]/2, ellip=ellip, 
+
+        Ltot = fluxes.Ltot(mag, m0)
+
+        sersic = Sersic2D(amplitude=1, r_eff=re, n=n, x_0=width[1]/2, y_0=width[0]/2, ellip=ellip, 
                           theta=pa)
         z = sersic(x, y)
+        z *= Ltot / np.nansum(z)
+        
         cutouts.append(z)
         cutout_data.append({"mag": mag, "n": n, "re": re})
     return Cutouts(cutouts=cutouts, cutout_data=cutout_data)
