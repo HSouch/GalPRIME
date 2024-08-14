9bd530b459599ac65b5d646cca8d53b8bbf9eb5b
HSouch <harrison.souchereau@yale.edu>
2024-08-14 17:04:00 -0300

    More stdout prints

--- galprime/sims.py
+++ galprime/sims.py
@@ -18,6 +18,9 @@ import multiprocessing as mp
 
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
+import warnings
+
+
 
 class GPrime:
 
@@ -31,9 +34,10 @@ class GPrime:
 
         self.log_level = kwargs.get("log_level", 20)
         self.logger = utils.setup_logging(self.run_id, self.log_level, 
-                                          log_filename=f'{c["DIRS"]["OUTDIR"]}output.log')
+                                          log_filename=f'{c["DIRS"]["OUTDIR"]}output_{self.run_id}.log')
         
-        self.logger.info(f"Starting run ID:{self.run_id}, GalPRIME Version: {gp.__version__}")
+        self.logger.info(f"Starting run ID:{self.run_id}, GalPRIME Version: {gp.__version__}", )
+        print(f"Starting run ID:{self.run_id}")
 
         # Load in all necessary files (backgrounds, psfs, catalogues, etc)
         self.bgs = cutouts.Cutouts.from_file(f'{c["FILE_DIR"]}{c["FILES"]["BACKGROUNDS"]}', 
@@ -55,7 +59,7 @@ class GPrime:
     def run(self, max_bins=None, verbose=True):
         c = self.config
 
-        binlist = binning.bin_catalogue(self.table, bin_params=c["BINS"], params=c["KEYS"], verbose=True)
+        binlist = binning.bin_catalogue(self.table, bin_params=c["BINS"], params=c["KEYS"], logger=self.logger)
         max_bins = len(binlist.bins) if max_bins is None else min(max_bins, len(binlist.bins))
 
         for i in range(max_bins):
@@ -66,41 +70,41 @@ class GPrime:
         return containers
 
 
-    def process_bin(self, bin, bin_id=0):
+    def process_bin(self, bin, bin_id=0, method=None):
         t_start = time.time()
         c = self.config
         kde = bin.to_kde()
-
-        models = modeling.gen_models(c, kde, mag_kde=self.mag_kde, n_models=c["MODEL"]["N_MODELS"])
-        containers = gen_containers(c, models, self.psfs, self.bgs)
 
         self.logger.info(f'Processing bin {bin_id}: {bin} on {c["NTHREADS"]} cores.')
 
         TIMEOUT = c["TIME_LIMIT"]
         results = []
+
         with ProcessPoolExecutor(max_workers=c["NTHREADS"]) as executor:
-            futures = [executor.submit(self.run_container, container) for container in containers]
-            try:
-                for future in as_completed(futures):
-                    try:
-                        data = future.result(timeout=TIMEOUT)
-                        results.append(data)
-                        logging.debug(f"  Finished processing container {data.id}")
-                    except TimeoutError:
-                        self.logger.warn(f"Timeout reached")
-                    except Exception as e:
-                        self.logger.warn(f"Error in container: {e}")
-            except TimeoutError:
-                self.logger.warn(f"Timeout reached for bin {bin_id}")
+            futures = [executor.submit(run_single_sersic, self, kde) for i in range(c["MODEL"]["N_MODELS"])]
+            for future in as_completed(futures):
+                try:
+                    data = future.result(timeout=TIMEOUT)
+                    results.append(data)
+                except TimeoutError:
+                    self.logger.warn(f"Timeout reached")
+                except Exception as e:
+                    self.logger.warn(f"Error in container: {e}")
 
-        self.logger.info(f"Finished processing bin {bin_id} in {(time.time() - t_start) / 60:.3f} minutes")
+        t_finish = time.time()
+        self.logger.info(f"Finished processing bin {bin_id} in {(t_finish - t_start) / 60:.3f} minutes")
+        self.logger.info(f'Time per object: {(t_finish - t_start) / c["MODEL"]["N_MODELS"]:.3f} seconds')
+        self.logger.info(f"{len(results)} successful extractions out of {c['MODEL']['N_MODELS']}")
 
-        return containers
+        print(results)
+
+        return None
     
 
     def run_container(self, container):
         container.run()
         return container
+
 
 
 
@@ -110,7 +114,7 @@ class GPrimeContainer:
         
         self.config = config
         self.model = model
-        self.convolved_model = self.bgadded = self.bgsub = None
+        self.convolved_model = self.bgadded = self.bg_model = None
         self.bgadded_masked, self.bgsub_masked = None, None
 
         self.logger = logging.getLogger(f"CONT_{self.id}")
@@ -123,14 +127,26 @@ class GPrimeContainer:
     
 
     def run(self):
-
+        
+        # Convolve model with PSF, add background
         self.convolved_model = convolve2d(self.model, self.psf, mode='same')
-
         self.bgadded = self.convolved_model + self.bg
 
+        # Generate source mask, and background mask/model
         self.source_mask, mask_metadata = masking.gen_mask(self.bgadded, self.config)
+        self.metadata.update(mask_metadata)
+        self.bg_source_mask, self.bg_model = gp.estimate_background_2D(self.bgadded, self.config)
 
-        
+        # Extract all necessary profiles
+        self.model_profile = gp.isophote_fitting(self.model, self.config)
+
+        self.bgmasked = np.copy(self.bgadded)
+        self.bgmasked[self.bg_source_mask] = self.bg_model.background[self.bg_source_mask]
+
+        # self.bgmasked = np.ma.masked_array(self.bgadded, mask=self.bg_source_mask)
+        self.bgadded_profile = gp.isophote_fitting(self.bgmasked, self.config)
+
+        self.logger.debug(len(self.model_profile), len(self.bgadded_profile))
 
         self.stop_code=1
 
@@ -169,3 +185,44 @@ def gen_containers(config, models, psfs, bgs):
                                           bg = np.copy(bgs.cutouts[bg_index]),
                                           id=i))
     return containers
+
+
+def run_single_sersic(gp_obj: GPrime, kde, plot=False):
+
+    warnings.filterwarnings("ignore")
+
+    psf_index = np.random.randint(len(gp_obj.psfs.cutouts))
+    bg_index = np.random.randint(len(gp_obj.bgs.cutouts))
+
+    psf, bg = np.copy(gp_obj.psfs.cutouts[psf_index]), np.copy(gp_obj.bgs.cutouts[bg_index])
+
+    model, model_data = gp.gen_single_sersic(gp_obj.config, kde, gp_obj.mag_kde)
+    convolved_model = convolve2d(model, psf, mode='same')
+    bgadded = convolved_model + bg
+
+    source_mask, mask_metadata = gp.masking.gen_mask(model, gp_obj.config)
+    bg_source_mask, bg_model = gp.estimate_background_2D(bgadded, gp_obj.config)
+
+    model_profile = gp.extraction.isophote_fitting(model, gp_obj.config)
+
+    bgmasked = np.copy(bgadded)
+    bgmasked[source_mask] = bg_model.background[source_mask]
+
+    bgadded_profile = gp.extraction.isophote_fitting(bgmasked, gp_obj.config)
+
+    bgsub = bgadded - bg_model.background
+    bgsub[source_mask] = 0
+    bgsub_profile = gp.extraction.isophote_fitting(bgsub, gp_obj.config)
+
+    return ProfileContainer(model_profile=model_profile, 
+                            bgadded_profile=bgadded_profile, 
+                            bgsub_profile=bgsub_profile, 
+                            metadata=mask_metadata)
+    
+class ProfileContainer:
+    def __init__(self, model_profile=None, bgadded_profile=None, bgsub_profile=None, metadata={}):
+        self.model_profile = model_profile
+        self.bgadded_profile = bgadded_profile
+        self.bgsub_profile = bgsub_profile
+        self.metadata = metadata
+
