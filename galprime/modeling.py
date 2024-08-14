61ddb3b7386e50b6e1da59e18a616d53197449d6
HSouch <harrison.souchereau@yale.edu>
2024-08-14 17:03:51 -0300

    Added single_sersic generation method

--- galprime/modeling.py
+++ galprime/modeling.py
@@ -13,6 +13,43 @@ def check_sersic(*args):
     return good
 
 
+def gen_single_sersic(config, kde, mag_kde=None, check_method=check_sersic, **kwargs):
+    max_tries = kwargs.get("ntries", 100)
+    arcconv = float(config["MODEL"]["ARCCONV"])
+    size = float(config["MODEL"]["SIZE"])
+
+    zpm = float(config["MODEL"]["ZPM"])
+    
+    xs, ys = np.mgrid[:size, :size]
+
+    try_index = 0
+    while try_index < max_tries:
+        mag, r_eff, n, ellip = kde.resample(size=1)
+        mag, r_eff, n, ellip = mag[0], r_eff[0], n[0], ellip[0]
+        
+        if check_method(mag, r_eff, n, ellip):
+            break
+        else:
+            try_index += 1
+    if mag_kde is not None:
+        mag = mag_kde.resample(size=1)[0]
+    
+    r_eff_pix = r_eff / arcconv
+    
+    Ltot = utils.Ltot(mag, zpm)
+
+    theta = np.random.uniform(0, 2 * np.pi)
+
+    model = Sersic2D(amplitude=1, r_eff=r_eff_pix, n=n, x_0=size/2, y_0=size/2, ellip=ellip, theta=theta)
+    z = model(xs, ys)
+
+    z *= Ltot / np.nansum(z)
+
+    model_info = {"MAG": mag, "R50": r_eff, "N": n, "ELLIP": ellip, "R50_PIX": r_eff_pix}
+    
+    return z, model_info
+
+
 
 def gen_models(config, kde, mag_kde=None, n_models=100, check_method=check_sersic, **kwargs):
     max_tries = kwargs.get("ntries", 100)
@@ -24,33 +61,11 @@ def gen_models(config, kde, mag_kde=None, n_models=100, check_method=check_sersi
     xs, ys = np.mgrid[:size, :size]
 
     models, models_info = [], []
-    for _ in range(n_models):
-        try_index = 0
-        while try_index < max_tries:
-            mag, r_eff, n, ellip = kde.resample(size=1)
-            mag, r_eff, n, ellip = mag[0], r_eff[0], n[0], ellip[0]
-            
-            if check_method(mag, r_eff, n, ellip):
-                break
-            else:
-                try_index += 1
-        if mag_kde is not None:
-            mag = mag_kde.resample(size=1)[0]
-        
-        r_eff_pix = r_eff / arcconv
-        
-        Ltot = utils.Ltot(mag, zpm)
 
-        theta = np.random.uniform(0, 2 * np.pi)
-
-        model = Sersic2D(amplitude=1, r_eff=r_eff_pix, n=n, x_0=size/2, y_0=size/2, ellip=ellip, theta=theta)
-        z = model(xs, ys)
-
-        z *= Ltot / np.nansum(z)
-
-        model_info = {"MAG": mag, "R50": r_eff, "N": n, "ELLIP": ellip, "R50_PIX": r_eff_pix}
-
+    for i in range(n_models):
+        z, model_info = gen_single_sersic(config, kde, mag_kde, check_method, ntries=max_tries)
         models.append(z)
         models_info.append(model_info)
+    
     
     return Cutouts(cutouts=models, cutout_data=models_info)
