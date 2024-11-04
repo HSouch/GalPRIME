779fab69fdee9737189314a3f0af994cbf203972
Harrison Souchereau <harrison.souchereau@yale.edu>
2024-11-04 02:41:34 -0500

    Added kde creation methods

--- galprime/models/kdes.py
+++ galprime/models/kdes.py
@@ -2,6 +2,65 @@ from ..utils import object_kde
 
 import numpy as np
 
+
+def setup_kde(model, config, table):
+    required_keys = model.required_keys()
+    config_keys = config.keys()
+    
+    # Check if all required keys are present in config
+    for key in required_keys:
+        if key not in config["KEYS"]:
+            raise ValueError(f"Missing key {key} in config")
+
+
+    columns = [table[config["KEYS"][key]] for key in required_keys]
+    kde = object_kde(columns)
+
+    return list(required_keys), kde
+
+
+def default_verifier(params):
+    good = params["REFF"] > 0               # Check that the effective radius is positive
+    good = good and 0 < params["N"] < 10    # Check that the Sersic index is within a reasonable range
+    good = good and params["MAG"] > 0       # Check that the magnitude is positive (abs. magnitude)
+
+    return good
+
+
+def sample_kde(config, keys, kde, verifier=default_verifier, attempts=100):
+    verified = False
+
+    while not verified and attempts > 0:
+
+        sample = kde.resample(size=1).transpose()[0]
+        params = dict(zip(keys, sample))
+        verified = verifier(params)
+        attempts -= 1
+    
+    if not verified:
+        raise ValueError(f"Failed to sample valid parameters after {attempts} attempts")
+
+    params = update_required(params, config)
+
+    return params
+
+
+def update_required(params, config):
+    """
+    Updates the 'params' dictionary with values from the 'config' dictionary.
+    Args:
+        params (dict): The dictionary to be updated.
+        config (dict): The configuration dictionary containing the new values.
+    Returns:
+        dict: The updated 'params' dictionary.
+    """
+
+    params["SHAPE"] = config["MODEL"]["SIZE"]   # Update the size of the model
+    params["M0"] = config["MODEL"]["ZPM"]       # Update the zero-point magnitude
+
+    return params
+
+
 class SyntheticKDE:
     def __init__(self, kde=None, **kwargs):
         self.kde = kde
