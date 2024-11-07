5a07ac6ba1ec97008ec5ea0ed24137ae33602c5f
Harrison Souchereau <harrison.souchereau@yale.edu>
2024-11-07 02:21:12 -0500

    Adjustments to logging time code

--- galprime/utils/handling.py
+++ galprime/utils/handling.py
@@ -7,6 +7,7 @@ def setup_logging(run_id, log_level, log_filename=None):
     if os.path.isfile(log_filename):
         os.remove(log_filename)
     logging.basicConfig(filename=log_filename, level=log_level,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        datefmt='%H:%M:%S')
 
     return logger
