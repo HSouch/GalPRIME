c4ee48d047a2e74874b35557cf20b601cd9218ba
HSouch <harrison.souchereau@yale.edu>
2024-12-08 16:55:26 -0500

    Added requirements and moved version to setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
-import galprime
 from setuptools import setup, find_packages
 
+__version__ = "0.1.1"
+
 setup(
-    name='galprime',
-    version=galprime.__version__,
-    description='GALaxy Profile Recovery from Images of Model Emission',
-    author='Harrison Souchereau',
-    author_email='harrison.souchereau@yale.edu',
-    url='https://github.com/hsouch/galprime',
+    name="galprime",
+    version=__version__,
+    description="GALaxy Profile Recovery from Images of Model Emission",
+    author="Harrison Souchereau",
+    author_email="harrison.souchereau@yale.edu",
+    url="https://github.com/hsouch/galprime",
     packages=find_packages(),
     install_requires=[
-        # Add your dependencies here
+        "astropy>=5",
+        "photutils>=1.10",
+        "numpy>=1.26",
+        "scikit-image>=0.19",
+        "configobj",
+        "pebble",
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     scripts=[
-        'bin/run_galprime',
-    ]
+        "bin/run_galprime",
+    ],
 )
