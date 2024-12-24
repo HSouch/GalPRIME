cf5a1b74cd554088f9523ac5dfcd1cf3e0aff980
HSouch <harrison.souchereau@yale.edu>
2024-12-23 20:41:17 -0400

    Docs update

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
+html_logo = "_static/gprime_logo_nobg.png"
 
 html_sidebars = {
     "**": ["search-field.html", "sidebar-nav-bs.html", "sidebar-ethical-ads.html"]
