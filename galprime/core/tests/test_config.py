2e24f4c643d95894a8fca480917d5e714e274efe
HSouch <harrison.souchereau@yale.edu>
2024-12-28 00:34:11 -0400

    Added more tests for configuration files

--- galprime/core/tests/test_config.py
+++ galprime/core/tests/test_config.py
@@ -1,12 +1,16 @@
 from .. import config
 
+from configobj import ConfigObj
+
 
 def test_default_config():
     default_config  = config.default_config()
+    assert isinstance(default_config, ConfigObj)
 
 
 def test_dump_default_config_file(tmpdir):
     filename = f"{tmpdir}default.gprime"
     config.dump_default_config_file(outname=filename)
 
-    config.read_config_file(filename)
+    test_read_config = config.read_config_file(filename)
+    assert isinstance(test_read_config, ConfigObj)
