5684f6ee9468afcc0fbc7d270e1cc552a59f7eeb
HSouch <harrison.souchereau@yale.edu>
2024-12-28 00:33:37 -0400

    Separated kde functions from synthetic kde models

--- galprime/models/__init__.py
+++ galprime/models/__init__.py
@@ -1,6 +1,7 @@
 from .backgrounds import *      # Synthetic background models
 from .galaxies import *         # Galaxy models
-from .kdes import *             # Synthetic KDE distributions
+from .kdes import *
+from .kde_models import *             # Synthetic KDE distributions
 from .psfs import *             # PSF models
 from .utils import *            # Utility functions
 from .verifiers import *        # Parameter verifiers
