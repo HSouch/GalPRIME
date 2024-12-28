a1b545566a87ecc7350d3f13f08d80f121c59042
HSouch <harrison.souchereau@yale.edu>
2024-12-28 04:09:30 -0400

    Added additional testing of verifiers

--- galprime/models/tests/helpers.py
+++ galprime/models/tests/helpers.py
@@ -29,12 +29,14 @@ class ModelTestBase:
     def test_default_gen(self):
         model_gen, model_params = self.model().generate()
 
-        assert model_gen is not None
-        assert isinstance(model_gen, np.ndarray)
-        assert len(model_gen.shape) == 2
+        assert model_gen is not None                # Check that the generated model is not None
+        assert isinstance(model_gen, np.ndarray)    # Check that the generated model is a numpy ndarray
+        assert len(model_gen.shape) == 2            # Check that the generated model is 2D
 
-        assert model_params is not None
-        assert isinstance(model_params, dict)
+        assert model_params is not None             # Check that the parameters are not None
+        assert isinstance(model_params, dict)       # Check that the parameters are a dictionary
+
+        assert np.nanmin(model_gen) >= 0            # Check that all values are non-negative
     
     def test_different_size(self):
         # Test that the model can be generated with different sizes
@@ -65,3 +67,44 @@ class ModelTestBase:
         # Check that each successive model is of lower total brightness
         for i in range(1, len(model_mags)):
             assert np.sum(model_mags[i]) < np.sum(model_mags[i-1])
+    
+    def test_verifier_exists(self):
+        # Test that the model has a verifier connected to it
+        assert self.model().verifier is not None
+
+    def test_verify_defaults(self):
+        # Test that the model verifier can successfully verify the default parameters
+        mod = self.model()
+        params = {**mod.defaults, "MAG": 20}
+        assert mod.verifier.verify(params)
+
+    def test_verify_bad_params(self):
+        # Test that the model verifier can successfully identify bad parameters
+        mod = self.model()
+        params = {**mod.defaults, "MAG": -1}
+        assert not mod.verifier.verify(params)
+        
+        params = {**mod.defaults, "REFF": -1}
+        assert not mod.verifier.verify(params)
+
+        params = {**mod.defaults, "N": 20}
+        assert not mod.verifier.verify(params)
+
+        params = {**mod.defaults, "ELLIP": -1}
+        assert not mod.verifier.verify(params)
+
+        params = {**mod.defaults, "ELLIP": 2}
+        assert not mod.verifier.verify(params)
+
+
+class KDETestBase:
+    """
+    KDETestBase is a base class for testing builtin GalPRIme KDE models.
+    Attributes:
+        name (str): The name of the model.
+        model (class): The model
+    """ 
+    name = None
+    kde = None
+
+    
